b0feb90bfbd53eb79005403adf7ffc06a17890fb
Isloot98 <isasloot@hotmail.com>
2024-01-22 11:25:29 +0000

    tiger image thing

---
+++ script.js
@@ -0,0 +1,22 @@
+    const accessKey = 'QWFxqc-GzSUSNUUSSq9Y2CA0ahEURdbVJVCnImj-x-Y'
+    const imageContainer = document.querySelector('.img-container');
+    
+    const form = document.getElementById('form');
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        let query = e.target.input.value;
+        search(query);
+    })
+
+
+const search = async (queryParam) => {
+    let response = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${queryParam}&client_id=${accessKey}`)
+    console.log(response);
+    let data = await response.json();
+    console.log(data.results);
+
+    let img = document.createElement('img');
+    img.src = data.results[0].urls.regular;
+    imageContainer.appendChild(img);
+
+}
